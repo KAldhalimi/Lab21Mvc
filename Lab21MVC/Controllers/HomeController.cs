5ac0bb47b3912939580760d36217f6e6f6e18894
KAldhalimi <karrarmajed@gmail.com>
2019-07-14 13:56:01 -0400

    initial commit

---
+++ Lab21MVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab21MVC.Models;
+
+
+namespace Lab21MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult NewUser(NewUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.User = user;
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        
+
+        }
+    }
